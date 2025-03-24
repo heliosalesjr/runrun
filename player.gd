ee1772adbb166db339bbd01b7f18c362514f2b95
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-03-24 15:29:26 -0300

    character basic movements

--- player.gd
+++ player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+func _physics_process(delta: float) -> void:
+	velocity.y += 1000.0 * delta
+	
+	velocity.x += 200.0 * delta
+	velocity.x = min(velocity.x, 100)
+	
+	
+	if is_on_floor() and Input.is_action_just_pressed("ui_up"):
+		velocity.y = -350.0
+	
+	move_and_slide()
